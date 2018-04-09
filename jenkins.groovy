85d674279444453aff2a8e7122c5b628962995b7
rohitjain994@gmail.com <rohitjain994@gmail.com>
2018-04-09 18:47:34 +0530

    sudo permission

--- jenkins.groovy
+++ jenkins.groovy
@@ -31,7 +31,7 @@ pipeline {
                 parallel{
                     stage ('Deploy to Staging'){
                         steps {
-    							sh "scp -i /home/ec2-user/SRID.pem -o StrictHostKeyChecking=no **/target/*.war ec2-user@${params.tomcat_dev}:/home/ec2-user/apache-tomcat-8.0.50/webapps"
+    							sh "sudo scp -i /home/ec2-user/SRID.pem -o StrictHostKeyChecking=no **/target/*.war ec2-user@${params.tomcat_dev}:/home/ec2-user/apache-tomcat-8.0.50/webapps"
     							sh "ssh -i /home/ec2-user/SRID.pem -o StrictHostKeyChecking=no ec2-user@${params.tomcat_dev} sudo sh /home/ec2-user/apache-tomcat-8.0.50/bin/shutdown.sh"
     							sh "ssh -i /home/ec2-user/SRID.pem -o StrictHostKeyChecking=no ec2-user@${params.tomcat_dev} sudo sh /home/ec2-user/apache-tomcat-8.0.50/bin/startup.sh"
     						
